6da16e2769b46eb3af538d0a7173319a05ee78a1
Dorian <dorian.abbadessa@laplateforme.io>
2025-02-03 20:39:42 +0100

    test: créer une classe avec un attribut vide et un autre plein. Ajouter +1 ans a l'animalet nommer l'animal

--- day1/job05/index.php
+++ day1/job05/index.php
@@ -1,5 +1,28 @@
 <?php
 
-class Personnage {
-    public string
+class Animal {
+    public string $age;
+    public string $prenom;
+
+    function __construct(?string $age = "0", ?string $prenom = "") {
+        $this->age = $age;
+        $this->prenom = $prenom;
+    }
+
+    function Viellir() {
+        $this->age += 1;
+    }
+
+    function Nommer() {
+        $this->prenom = "Luna";
+    }
 }
+
+$animal = new Animal();
+echo "L'age de l'animal est de " . $animal->age . " an<br>";
+
+$animal->Viellir();
+echo "L'age de l'animal est désormais " . $animal->age . " an<br>";
+
+$animal->Nommer();
+echo "L'animal se nomme " . $animal->prenom;
